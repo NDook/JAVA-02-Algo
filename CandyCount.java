b3ea3869f8f32809b24e38c87e327d1754f42134
NDook <thomasmludek@gmail.com>
2022-11-07 14:40:16 +0100

    ok

--- CandyCount.java
+++ CandyCount.java
@@ -1,14 +1,15 @@
-class CandyCount {
-
+public class CandyCount {
     public static void main(String[] args) {
-        double realMoney = 12.4;
-        double realPrice = 1.2;
-        int entiesCandies = 0;
-
-    while(realMoney > 0 && realPrice > 0) {
-            entiesCandies += 1;
-            realMoney = realMoney - realPrice;
+        double money = 12.4;
+        double price = 1.2;
+        int caddies = 0;
+        if  (money > 0 && price >= 0) {
+            while (money - price >= 0)
+            {
+                caddies = caddies + 1;
+                money = money - price;
+            }
         }
-    System.out.println(entiesCandies);
-    }
+        System.out.println(caddies);
+    }    
 }
